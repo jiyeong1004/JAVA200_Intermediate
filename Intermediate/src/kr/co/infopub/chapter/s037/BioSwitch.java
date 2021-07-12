41bf112521519690538408143424f6bb4a2ce8c7
s2019s28 <s2019s28@e-mirim.hs.kr>
2021-07-13 00:05:19 +0900

    037. 상수값에 따라 신체, 감정, 지성값 출력하기

---
+++ Intermediate/src/kr/co/infopub/chapter/s037/BioSwitch.java
@@ -0,0 +1,29 @@
+// 037. 상수값에 따라 신체, 감정, 지성값 출력하기
+package kr.co.infopub.chapter.s037;
+
+// 상수, switch case
+public class BioSwitch {
+	// 상수
+	public static final int PHYSICAL = 23;
+	public static final int EMOTIONAL = 28;
+	public static final int INTELLECTUAL = 33;
+	
+	// 메서드 선언
+	public static String textInfor(int index, double value) {
+		String result = "";
+		switch(index) {
+		case PHYSICAL : result = "신체 지수 : "; break;
+		case EMOTIONAL : result = "감정 지수 : "; break;
+		case INTELLECTUAL : result = "지성 지수 : "; break;
+		default : result = "미결정 : "; break;
+		}
+		return result + (value * 100);
+	}
+
+	public static void main(String[] args) {
+		int index = PHYSICAL;
+		double value = 0.86;
+		String st = textInfor(index, value);
+		System.out.println(st);
+	}
+}
