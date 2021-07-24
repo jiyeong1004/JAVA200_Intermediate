15892e53dca1d2389d177472e09355dae73b6a05
s2019s28 <s2019s28@e-mirim.hs.kr>
2021-07-25 00:36:02 +0900

    049. 제곱근을 이용하여 소수 판별하기

---
+++ Intermediate/src/kr/co/infopub/chapter/s049/PrimMath.java
@@ -0,0 +1,26 @@
+// 049. 제곱근을 이용하여 소수 판별하기
+package kr.co.infopub.chapter.s049;
+
+// Math 클래스 메서드
+public class PrimMath {
+	public static boolean isPrime(int n) {
+		boolean isS = true;
+		for(int i = 2; i <= (int)Math.sqrt(n); i++) {
+			if(n % i == 0) {
+				isS = false;
+				break;
+			}
+		}
+		return isS;
+	}
+
+	public static void main(String[] args) {
+		int number = 1234567;
+		boolean ifPrime = isPrime(number);  // 소수(Prime)인가를 확인한다.
+		if(ifPrime) {
+			System.out.printf("%d는 1과 자신으로만 나눠떨어지는 소수다.", number);
+		} else {
+			System.out.printf("%d는 소수가 아니다.", number);
+		}
+	}
+}
