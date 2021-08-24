0e2a87c3a71352dcdf9d2c81569285861a39754e
s2019s28 <s2019s28@e-mirim.hs.kr>
2021-08-25 00:17:41 +0900

    080. 박싱, 언박싱으로 기본 타입을 참조 타입으로 변환하기

---
+++ Intermediate/src/kr/co/infopub/chapter/s080/EasyBoxingMain.java
@@ -0,0 +1,31 @@
+// 080. 박싱, 언박싱으로 기본 타입을 참조 타입으로 변환하기
+package kr.co.infopub.chapter.s080;
+
+import java.util.ArrayList;
+
+// Boxing / Unboxing
+public class EasyBoxingMain {
+
+	public static void main(String[] args) {
+		int intNm1 = 123;
+		long longNum1 = 345L;
+		double doubleNum = 123.123;
+		Integer intWrap1 = intNm1;  // new Integer(intNm1);
+		Long longWrap1 = longNum1;  // new Long(longNum1);
+		Double doubleWrap1 = doubleNum;  // new Double(doubleNum);
+		double dd = doubleWrap1;
+		// 기본 타입 <--> Wrapper는 캐스팅 없이 사용
+		Integer intWrap2 = intNm1;  // Boxing
+		intNm1 = intWrap2;  // UnBoxing
+		System.out.println(intNm1);
+		
+		ArrayList<Integer> ilists = new ArrayList<>();
+		ilists.add(new Integer(3));
+		ilists.add(5);
+		int a = ilists.get(0);  // <Integer>
+		System.out.println(a);
+		
+		int b = Integer.parseInt("123");
+		System.out.println(b);
+	}
+}
